26f0cd3fa47062f19c13d9000d4014887dd931f6
Indy Hurt <indy.hurt@gmail.com>
2016-01-13 09:40:35 -0800

    update to queries to exclude relations

--- queries/fitness_sql.sql
+++ queries/fitness_sql.sql
@@ -1,21 +1,21 @@
 ﻿drop table if exists fitness;
 create table fitness (query_name text, value real);
 
-insert into fitness (query_name, value) values ('fitness_points', (select count(*) from planet_osm_point where amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')));
+insert into fitness (query_name, value) values ('fitness_points', (select count(*) from planet_osm_point where amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym') and osm_id > 0));
 
-insert into fitness (query_name, value) values ('fitness_polygons', (select count(*) from planet_osm_polygon where amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')));
+insert into fitness (query_name, value) values ('fitness_polygons', (select count(*) from planet_osm_polygon where amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym') and osm_id > 0));
 
-insert into fitness (query_name, value) values ('fitness_points_with_housenumber', (select count(*) from planet_osm_point where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym'))  and "addr:housenumber" is not null));
+insert into fitness (query_name, value) values ('fitness_points_with_housenumber', (select count(*) from planet_osm_point where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')) and osm_id > 0 and "addr:housenumber" is not null));
 
-insert into fitness (query_name, value) values ('fitness_polygons_with_housenumber', (select count(*) from planet_osm_polygon where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym'))  and "addr:housenumber" is not null));
+insert into fitness (query_name, value) values ('fitness_polygons_with_housenumber', (select count(*) from planet_osm_polygon where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')) and osm_id > 0 and "addr:housenumber" is not null));
 
-insert into fitness (query_name, value) values ('fitness_points_with_website', (select count(*) from planet_osm_point where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym'))  and (tags ? 'website' or tags ? 'url')));
+insert into fitness (query_name, value) values ('fitness_points_with_website', (select count(*) from planet_osm_point where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')) and osm_id > 0 and (tags ? 'website' or tags ? 'url')));
 
-insert into fitness (query_name, value) values ('fitness_polygons_with_website', (select count(*) from planet_osm_polygon where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym'))  and (tags ? 'website' or tags ? 'url')));
+insert into fitness (query_name, value) values ('fitness_polygons_with_website', (select count(*) from planet_osm_polygon where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')) and osm_id > 0 and (tags ? 'website' or tags ? 'url')));
 
-insert into fitness (query_name, value) values ('fitness_points_with_hours', (select count(*) from planet_osm_point where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym'))  and (tags ? 'opening_hours' or tags ? 'operating_hours')));
+insert into fitness (query_name, value) values ('fitness_points_with_hours', (select count(*) from planet_osm_point where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')) and osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')));
 
-insert into fitness (query_name, value) values ('fitness_polygons_with_hours', (select count(*) from planet_osm_polygon where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym'))  and (tags ? 'opening_hours' or tags ? 'operating_hours')));
+insert into fitness (query_name, value) values ('fitness_polygons_with_hours', (select count(*) from planet_osm_polygon where (amenity in ('fitness_center', 'fitness_centre', 'fitness', 'gym') or leisure in ('fitness_center', 'fitness_centre', 'fitness') or sport in ('fitness', 'gym')) and osm_id > 0 and (tags ? 'opening_hours' or tags ? 'operating_hours')));
 
 insert into fitness (query_name, value) values ('fitness_total', (select sum(value) from fitness where query_name = 'fitness_points' or query_name = 'fitness_polygons'));
 
---
+++ queries/grocery_sql.sql
@@ -0,0 +1,34 @@
+﻿drop table if exists grocery;
+create table grocery (query_name text, value real);
+
+insert into grocery (query_name, value) values ('grocery_points', (select count(*) from planet_osm_point where shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0));
+
+insert into grocery (query_name, value) values ('grocery_polygons', (select count(*) from planet_osm_polygon where shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0));
+
+insert into grocery (query_name, value) values ('grocery_points_with_housenumber', (select count(*) from planet_osm_point where (shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0)  and "addr:housenumber" is not null));
+
+insert into grocery (query_name, value) values ('grocery_polygons_with_housenumber', (select count(*) from planet_osm_polygon where (shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0)  and "addr:housenumber" is not null));
+
+insert into grocery (query_name, value) values ('grocery_points_with_website', (select count(*) from planet_osm_point where (shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0)  and (tags ? 'website' or tags ? 'url')));
+
+insert into grocery (query_name, value) values ('grocery_polygons_with_website', (select count(*) from planet_osm_polygon where (shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0)  and (tags ? 'website' or tags ? 'url')));
+
+insert into grocery (query_name, value) values ('grocery_points_with_hours', (select count(*) from planet_osm_point where (shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0)  and (tags ? 'opening_hours' or tags ? 'operating_hours')));
+
+insert into grocery (query_name, value) values ('grocery_polygons_with_hours', (select count(*) from planet_osm_polygon where (shop in ('supermarket', 'greengrocer', 'grocery', 'convenience') or building = 'supermarket' and osm_id > 0)  and (tags ? 'opening_hours' or tags ? 'operating_hours')));
+
+insert into grocery (query_name, value) values ('grocery_total', (select sum(value) from grocery where query_name = 'grocery_points' or query_name = 'grocery_polygons'));
+
+insert into grocery (query_name, value) values ('grocery_with_address_total', (select sum(value) from grocery where query_name = 'grocery_points_with_housenumber' or query_name = 'grocery_polygons_with_housenumber'));
+
+insert into grocery (query_name, value) values ('grocery_with_website_total', (select sum(value) from grocery where query_name = 'grocery_points_with_website' or query_name = 'grocery_polygons_with_website'));
+
+insert into grocery (query_name, value) values ('grocery_with_hours_total', (select sum(value) from grocery where query_name = 'grocery_points_with_hours' or query_name = 'grocery_polygons_with_hours'));
+
+insert into grocery (query_name, value) values ('grocery_with_address_pct', (((select value from grocery where query_name = 'grocery_with_address_total')/(select value from grocery where query_name = 'grocery_total')) *100));
+
+insert into grocery (query_name, value) values ('grocery_with_website_pct', (((select value from grocery where query_name = 'grocery_with_website_total')/(select value from grocery where query_name = 'grocery_total')) *100));
+
+insert into grocery (query_name, value) values ('grocery_with_hours_pct', (((select value from grocery where query_name = 'grocery_with_hours_total')/(select value from grocery where query_name = 'grocery_total')) *100));
+
+select * from grocery
