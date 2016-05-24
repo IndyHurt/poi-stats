9ddef02c1ccc1af13fac5b19667a8ceab5ee0c50
Indy Hurt <indy.hurt@gmail.com>
2016-05-24 13:07:19 -0700

    update bike queries

--- queries/bicycle_sql.sql
+++ queries/bicycle_sql.sql
@@ -1,10 +1,10 @@
 ﻿drop table if exists bicycle;
 create table bicycle (query_name text, value real);
 
--- what's the length of bike paths separate from the road?
-insert into bicycle (query_name, value) values ('bike_track', (select sum(st_length(way))/1000 from planet_osm_line where (highway = 'cycleway' and osm_id > 0) or (highway = 'cycleway' and tags @> '"cycleway"=>"track"' and osm_id > 0)));
+-- what's the length of bike tracks which are paths separate from the road?
+insert into bicycle (query_name, value) values ('bike_track', (select sum(st_length(way))/1000 from planet_osm_line where (highway = 'cycleway' and osm_id > 0) or (highway is not null and tags @> '"cycleway"=>"track"' and osm_id > 0) or (highway is not null and bicycle = 'use_sidepath')));
 
--- what's the length of bike paths on the road?
+-- what's the length of bike lanes which are paths on the road?
 insert into bicycle (query_name, value) values ('bike_lane', (select sum(st_length(way))/1000 from planet_osm_line where tags ? 'cycleway' and highway is not null and osm_id > 0));
 
 -- what's the length of bike routes
